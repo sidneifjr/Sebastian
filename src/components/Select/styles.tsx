b36ece7b94a92d4a33c8fdaf322d004a63d8c42f
Sidnei Farias <sidneijr.77@gmail.com>
2022-08-02 22:31:27 -0300

    Finishes styles of the Select component

--- src/components/Select/styles.tsx
+++ src/components/Select/styles.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
+import arrowDown from '../../assets/select-arrow.svg';
 
 const SelectItem = styled.select`
   width: 100%;
   max-width: 500px;
-  background: #ffffff;
+  background: #ffffff url(${arrowDown}) no-repeat center;
+  background-position-x: 96.5%;
   border: solid 1px #0D0D0D;
   border-radius: 5px;
   padding: 15px 20px;
